487911bcd83522c7c9c9187eafef0bd0c4c7d973
Naeem Mansuri <mansurinaeem000@gmail.com>
2021-11-29 14:14:57 +0530

    jasmine_sprout: Initialize Evolution X 5.x

---
+++ evolution_jasmine_sprout.mk
@@ -0,0 +1,64 @@
+#
+# Copyright (C) 2018 The Xiaomi-SDM660 Project
+# Copyright (C) 2020-2021 The Evolution X Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
+
+# Inherit Evolution-X product configuration
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+# Define first api level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Build Description
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="jasmine-user 9 PKQ1.180904.001 V10.0.9.0.PDIMIXM release-keys" \
+    PRODUCT_NAME="jasmine_sprout" \
+    TARGET_DEVICE="jasmine_sprout"
+
+# Build Fingerprint
+BUILD_FINGERPRINT := "xiaomi/wayne/wayne:8.1.0/OPM1.171019.011/V9.5.11.0.ODCCNFA:user/release-keys"
+
+# Density
+TARGET_SCREEN_DENSITY := 360
+
+# Inherit Evolution-X build stuff.
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Gapps
+USE_GAPPS := true
+IS_PHONE := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+
+# Device identifier
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := evolution_jasmine_sprout
+PRODUCT_DEVICE := jasmine_sprout
+PRODUCT_MODEL := Mi A2
