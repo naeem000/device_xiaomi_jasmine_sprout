65d2be7b469d42266157a1c083f7defd73ba6745
Naeem Mansuri <mansurinaeem000@gmail.com>
2021-12-28 17:14:33 +0530

    jasmine_sprout: update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,6 @@
+rm -rf hardware/qcom-caf/sdm660/display
+rm -rf hardware/qcom-caf/sdm660/media
+rm -rf hardware/qcom-caf/sdm660/audio
 git clone https://github.com/naeem000/android_hardware_qcom-caf_display.git -b 12 hardware/qcom-caf/sdm660/display
 git clone https://github.com/naeem000/android_hardware_qcom-caf_media.git -b 12 hardware/qcom-caf/sdm660/media
 git clone https://github.com/naeem000/android_hardware_qcom-caf_audio.git -b 12 hardware/qcom-caf/sdm660/audio
