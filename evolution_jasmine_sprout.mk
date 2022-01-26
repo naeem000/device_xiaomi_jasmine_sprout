96658cd0eb57d255a1b7252585514ccdccbc81a9
naeem000 <mansurinaeem000@gmail.com>
2022-01-26 18:07:39 +0530

    jasmine_sprout: Update Raven FP to Jan

--- evolution_jasmine_sprout.mk
+++ evolution_jasmine_sprout.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Build Description
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.211205.016.A1/7957957 7957957 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220105.007 8030436 release-keys"
 
 # Build Fingerprint
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.211205.016.A1/7957957:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys"
 
 # Device identifier
 PRODUCT_BRAND := Xiaomi
