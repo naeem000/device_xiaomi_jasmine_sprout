54ddf2aaacee4c8b0604a365f9c420f9d07a2e4e
OdSazib <odsazib@gmail.com>
2022-01-27 06:47:36 +0600

    jasmine_sprout: bring changes for aosp
    Change-Id: Iae3e88666609bea345ed760882dce180a5f3aca4

--- device.mk
+++ device.mk
@@ -38,6 +38,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     JasmineWifi
 
+# PRODUCT_SHIPPING_API_LEVEL indicates the first api level, device has been commercially launched on.
+PRODUCT_SHIPPING_API_LEVEL := 27
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
