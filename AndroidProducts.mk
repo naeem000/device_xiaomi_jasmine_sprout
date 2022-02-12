b26a914b54a84601eddf6dd91c28d1d456758e2d
Naeem Mansuri <mansurinaeem000@gmail.com>
2022-02-12 12:07:22 +0530

    jasmine_sprout: Bringup PixelBlaster

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2018-2021 PixelBlaster
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# inherit MI A2 device config for PixelBlaster
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_jasmine_sprout.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_jasmine_sprout-user \
+    aosp_jasmine_sprout-userdebug \
+    aosp_jasmine_sprout-eng
